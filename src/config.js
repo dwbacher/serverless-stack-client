7e9c3fbeaaee4468f353e71b990d8f2748e3ab78
David Bacher <david.bacher@alchemysystems.com>
2020-08-21 12:37:07 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "dwb-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://jpsxaai075.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_Qd4qTkxrZ",
+    APP_CLIENT_ID: "22bgrvbei2i2k6ls610f7miep",
+    IDENTITY_POOL_ID: "us-east-1:178b53b4-6cfc-41cb-88ac-bc4c286912b2D"
+  }
+};
