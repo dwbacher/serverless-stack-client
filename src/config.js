48aa6069a316de8883fbc6ded4899c58d1ea5d93
David Bacher <david.bacher@alchemysystems.com>
2020-09-04 11:25:08 -0500

    Building our Notes client React app

--- src/config.js
+++ src/config.js
@@ -11,6 +11,7 @@ export default {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_Qd4qTkxrZ",
     APP_CLIENT_ID: "22bgrvbei2i2k6ls610f7miep",
-    IDENTITY_POOL_ID: "us-east-1:178b53b4-6cfc-41cb-88ac-bc4c286912b2D"
-  }
+    IDENTITY_POOL_ID: "us-east-1:178b53b4-6cfc-41cb-88ac-bc4c286912b2"
+	},
+	STRIPE_KEY: "pk_test_51HFNa3Iu8bN42HTQDSskOej5uVBc4mB1vb4ZFhwzHyfwQjx3AqlQhIoDnU2yteTKVo31Ie4CVljbAbedBLa7MIMp00uxt7OOG1",
 };
